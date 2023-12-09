b226935c4495e78931b82e06126d22a970f7f46a
Yiqian Yang <yiqianya@usc.edu>
2023-12-08 20:49:27 -0800

    show/hide password

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,19 @@
+import React, { useEffect, useState } from "react";
+
 export default function LoginPage() {
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState('password');
+
+  const handleToggle = () => {
+    console.log(password);
+    if (type == 'password'){      
+      setType('text');
+    }
+    else {
+      setType('password');
+    }
+  }
+
     return (
       <div className="w-screen h-screen flex flex-col">
         <header className='w-full mx-auto p-2 flex justify-between bg-black'>
@@ -7,7 +22,7 @@ export default function LoginPage() {
             <span className="text-white text-sm font-bold">Chuwa</span>
           </a>
         
-          <div className="sm:flex w-1/3 items-center bg-white px-1 rounded-sm overflow-hidden inset-0">
+          <div className="sm:flex w-1/3 items-center bg-white px-1 rounded-sm overflow-hidden">
             <input className="text-base text-gray-400 flex-grow outline-none px-2 py-2" type="search" name="search" placeholder="Search" />
             <div className="ms:flex items-center px-2 space-x-4 mx-auto">
               <button type="submit" className="flex">
@@ -42,23 +57,34 @@ export default function LoginPage() {
                 <form className="bg-white shadow-lg rounded px-10 pt-6 pb-8 mb-4">
                   <label className="flex text-3xl font-bold justify-center mt-8">Sign in to your account</label>
                   <div className="mt-6 mb-4">
-                    <label className="block text-gray-500 text-sm mb-2" for="email">
+                    <label className="block text-gray-500 text-sm mb-2">
                       Email
                     </label>
-                    <input className="appearance-none border rounded w-full py-4 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="you@example.com" />
+                    <input className="border rounded w-full py-4 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="you@example.com" />
                   </div>
                   <div className="mb-2">
-                    <label className="block text-gray-500 text-sm mb-2" for="password">
+                    <label className="block text-gray-500 text-sm mb-2">
                       Password
                     </label>
-                    <input className="appearance-none border border-red-500 rounded w-full py-4 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="•••••••••••••••••••"/>
+                    <div className="relative w-full">
+                      <input className="border flex-grow border-red-500 rounded w-full py-4 px-3 text-black mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" placeholder="•••••••••••••••••••"
+                        type={type}
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}/>
+                      <div className="absolute top-3 right-0 flex items-center pr-4">
+                        <button type="button" className="underline rounded px-2 py-1 text-sm text-gray-500 caret-transparent" onClick={handleToggle}>Show</button>
+                      </div>
+                    </div>
                     <p className="text-red-500 text-xs italic">Please enter your password.</p>
+                    
                   </div>
                   <div className="mb-4">
-                    <button className="flex w-full text-sm bg-[#5048e5] hover:bg-gray-500 text-white justify-center items-center py-3 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <button className="flex w-full font-bold text-sm bg-[#5048e5] hover:bg-gray-500 text-white justify-center items-center py-3 rounded focus:outline-none focus:shadow-outline" type="button">
                         Sign In
                     </button>
                   </div>
+
                   <div className="flex items-center justify-between">
                     <span className="text-gray-500">
                       Don't have an account?  
