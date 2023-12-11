251cc97eeb59f117b11b24de84f92923f53e0106
Yiqian Yang <yiqianya@usc.edu>
2023-12-10 19:24:04 -0800

    mongodb setup

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
 const app = express();
+const Users = require("./models/User");
 
 app.use(express.json());
-
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+mongoose.connect(process.env.MONGODB_URL);
 
 app.get("/", (req, res) => {
   res.json("Hello World!");
@@ -12,6 +17,7 @@ app.get("/", (req, res) => {
 
 app.post("/signup", (req, res) => {
   const { email, password } = req.body;
+  Users.create({ email, password });
   res.json({ email, password });
 });
 
