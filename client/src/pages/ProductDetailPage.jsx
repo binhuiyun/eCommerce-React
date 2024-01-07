49a7ea78ece08dd7b81f0fea341c33546bca47ab
baby bin <binhuiyun@gmail.com>
2024-01-06 17:47:23 -0800

    detailPage done

--- client/src/pages/ProductDetailPage.jsx
+++ client/src/pages/ProductDetailPage.jsx
@@ -4,14 +4,23 @@ import GroupButtons from "./ProductDisplayScreen/ProductCard/GroupButtons";
 import { useState, useEffect} from "react";
 import { useSelector,useDispatch } from "react-redux";
 import { Layout } from "antd";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate} from "react-router-dom";
 import { getCurrentProduct } from "../services/productService";
+import { toggleEdit } from "../../reducers/editActions";
+
 
 export default function ProductDetailPage() {
   const {id } = useParams();
   const dispatch = useDispatch();
-  const productInfo = useSelector((state) => state.productItem);
+  const product = useSelector((state) => state.productItem);
   const userInfo = JSON.parse(localStorage.getItem("user"));
+  const edit = useSelector((state)=>state.edit);
+  const navigate = useNavigate();
+
+  const handleEdit = () => {
+    dispatch(toggleEdit(!edit)); 
+    navigate(`/edit-product/${product._id}`);
+  };
 
   useEffect(() => {  
     getCurrentProduct(id, dispatch);
@@ -28,39 +37,40 @@ export default function ProductDetailPage() {
         </h2>
         <div className="xs:w-[80%] md:w-[90%] flex xs:flex-col md:flex-row bg-white shadow-lg items-center">
           <div className="xs:mt-5 md:mt-0 xs:w-[90%] md:w-1/2 xs:h-[50%] md:h-[90%] xs:ml-0 md:ml-[5%] flex justify-center items-center">
-            <img src={`${productInfo.link}`} />
+            <img src={`${product.image}`} />
           </div>
           <div className="xs:w-[80%] md:w-[45%] h-full flex justify-center items-center">
             <div className="xs:w-[100%] md:w-[80%] h-[90%]">
               <h3 className="text-[#6B7280] xs:mt-2 md:mt-10 xs:text-sm md:text-lg">
-                {productInfo.category}
+                {product.category}
               </h3>
               <h2 className="xs:mt-2 md:mt-4 xs:text-md md:text-3xl font-bold text-black">
-                {productInfo.productName}
+                {product.name}
               </h2>
               <div className="flex flex-row">
-                <h2 className="xs:mt-4 md:mt-6 xs:text-md md:text-3xl font-bold text-black">{`$${productInfo.price}`}</h2>
-                {productInfo.quantity <= 0 && (
+                <h2 className="xs:mt-4 md:mt-6 xs:text-md md:text-3xl font-bold text-black">{`$${product.price}`}</h2>
+                {product.stockQuantity <= 0 && (
                   <div className="xs:mt-2 md:mt-6 xs:ml-2 md:ml-4 xs:w-[60px] md:w-[90px] xs:h-[30px] md:h-[30px] bg-pink-300 text-red-600 xs:text-[8px] md:text-xs flex items-center justify-center border border-pink-300 rounded-md">
                     Out of Stock
                   </div>
                 )}
               </div>
               <p className="text-[#6B7280] xs:mt-4 md:mt-6 xs:text-[10px] md:text-lg">
-                {productInfo.productDescription}
+                {product.description}
               </p>
               <div className="flex flex-row w-1/2 xs:mt-4 md:mt-8 mb-10">
                 <GroupButtons
                   productData={{
-                    productID: productInfo._id,
-                    productPrice: productInfo.price,
-                    productQuantity: productInfo.quantity,
-                    productTitle: productInfo.productName,
-                    productDescription: productInfo.productDescription,
-                    productCategory: productInfo.category,
+                    productID: product._id,
+                    productPrice: product.price,
+                    productQuantity: product.stockQuantity,
+                    productTitle: product.name,
+                    productDescription: product.description,
+                    productCategory: product.category,
                   }}
                 />
-                <button className="flex items-center justify-center w-1/2 text-base border border-[#6B7280] rounded-md ml-4 transition-colors duration-300 hover:bg-gray-300">
+                <button className="flex items-center justify-center w-1/2 text-base border border-[#6B7280] rounded-md ml-4 transition-colors duration-300 hover:bg-gray-300"
+                onClick={handleEdit}>
                   Edit
                 </button>
               </div>
